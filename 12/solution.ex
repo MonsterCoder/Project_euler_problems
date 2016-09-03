7056879548ebd96ed5a0755181ad44158e39bc9b
monstercoder <monstercoder@gmail.com>
2016-09-03 16:40:51 -0500

    solution1

--- 12/solution.ex
+++ 12/solution.ex
@@ -3,5 +3,17 @@ defmodule Triangular do
     Stream.unfold({0,1}, fn {acc, n} -> { acc+n, {acc+n, n+1} } end) 
   end
 
+  def run(n) do
+    generate
+    |> Stream.filter(&(Factor.of(&1) |> length > n))
+    |> Enum.at 0
+  end
+end
 
+defmodule Factor do
+    def of(1), do: [1];
+    def of(n) do
+       1..n      
+       |> Enum.filter(&(rem(n,&1)==0))
+    end
 end
--- 12/solution_tests.exs
+++ 12/solution_tests.exs
@@ -2,9 +2,20 @@ ExUnit.start()
 
 defmodule Solution_tests do
   use ExUnit.Case, async: true
-  import Triangular, only: [generate: 0 ]
+  import Triangular, only: [generate: 0, run: 1 ]
+  import Factor, only: [of: 1]
 
   test "returns correct trangular numbers" do
     assert generate |>  Enum.take(7) |> Enum.to_list() == [1, 3, 6, 10, 15, 21, 28]
+  end
+
+  test "find factors for a number" do
+    assert of(1)  == [1]
+    assert of(3)  == [1, 3]
+    assert of(28) == [1,2,4,7,14,28]
+  end
+  
+  test 'return 28 as the first of more than 5 divisors' do
+    assert run(5) == 28
   end
 end

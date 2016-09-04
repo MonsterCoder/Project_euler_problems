b6bcfbbc015e1572dda570b3c2671d3ac2113072
monstercoder <monstercoder@gmail.com>
2016-09-03 20:31:41 -0500

    solve in math way

--- 12/solution2.ex
+++ 12/solution2.ex
@@ -1,5 +1,37 @@
-defmodule Triangular2 do
-  def generate2 do
+defmodule Triangular do
+  # this method is not needed in this solution, just used to prove f(n) = n * (n+1) / 2
+  def generate do
     Stream.unfold(1, &({(&1 * (&1+1))  |> div(2), &1 +1}))
   end
+
+  def run(leng) do
+    Stream.unfold(1, &({&1, &1+1}))
+    |> Stream.map(&cal_length/1)
+    |> Enum.find_index(&(&1 > leng))
+    |> (fn(idx) -> (idx+1) *(idx+2) |> div(2) end).()
+  end
+  
+  def cal_length(1), do: 1
+
+  def cal_length(i) when rem(i,2)==0  do
+    p1 = (Factor.of(div(i,2)) |> length) -1
+    p2 = Factor.of(i+1) |> length
+
+    p1+p2 + (p2-1) * p1
+  end
+
+
+  def cal_length(i) do
+    p1 = Factor.of(i) |> length
+    p2 = (Factor.of(div(i+1, 2)) |> length) -1
+    p1+p2 + (p1-1) * p2 
+  end
+end
+
+defmodule Factor do
+    def of(1), do: [1]
+    def of(n) do
+      1..n 
+      |> Enum.filter(&(rem(n, &1)==0))
+    end
 end
--- 12/solution_tests.exs
+++ 12/solution_tests.exs
@@ -3,12 +3,10 @@ ExUnit.start()
 defmodule Solution_tests do
   use ExUnit.Case, async: true
   import Triangular, only: [generate: 0, run: 1 ]
-  import Triangular2, only: [generate2: 0]
   import Factor, only: [of: 1]
 
   test "returns correct trangular numbers" do
     assert generate |>  Enum.take(7) |> Enum.to_list() == [1, 3, 6, 10, 15, 21, 28]
-    assert generate2 |> Enum.take(7) |> Enum.to_list == [1,3,6,10,15,21,28]
   end
 
   test "find factors for a number" do
